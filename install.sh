dbffab993e55bcbe7f6ca190ea9b7ed50c350634
Alex <a1998d33@gmail.com>
2024-03-09 17:59:46 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,11 +17,13 @@ sudo apt-get update && sudo apt-get install -y zsh git curl wget sudo
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc --skip-chsh
 
 # Install Zsh plugins
-git clone https://github.com/MichaelAquilina/zsh-you-should-use.git $ZSH_CUSTOM/plugins/you-should-use
+git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/you-should-use
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 sed -i '/^plugins=/d' ~/.zshrc
+sed -i '/source $ZSH\/oh-my-zsh.sh/d' ~/.zshrc
 echo "plugins=(git zsh-autosuggestions zsh-syntax-highlighting you-should-use)" >> ~/.zshrc
+echo "source $ZSH/oh-my-zsh.sh" >> ~/.zshrc
 
 
 #Correct symbols/emoji display in Tmux
