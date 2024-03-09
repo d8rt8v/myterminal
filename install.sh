4656a09b3ea93d0d01d626d46c930c7270b9597d
Alex <a1998d33@gmail.com>
2024-03-09 16:23:02 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Define color codes
-GREEN='\033[0;32m\]'
-RED='\033[0;31m\]'
-NC='\033[0m\]' # No Color
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
 
 # Ask user of it's intentions
 while true; do
