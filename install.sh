d288da3edf4d567bb73eae9f507405a9a1ead316
Alex <a1998d33@gmail.com>
2024-03-09 16:11:21 +0300

    install curl git wget if not done

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ done
 
 
 # Install Zsh
-sudo apt-get update && sudo apt-get install -y zsh
+sudo apt-get update && sudo apt-get install -y zsh git curl wget
 
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
