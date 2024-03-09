a74bfabf550cd83cf329b40220b11e198eff7e11
Alex <a1998d33@gmail.com>
2024-03-09 18:07:36 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ sudo apt-get update && sudo apt-get install -y zsh git curl wget sudo
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc --skip-chsh
 
 # Install Zsh plugins
-git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/you-should-use
+git clone https://github.com/MichaelAquilina/zsh-you-should-use.git $ZSH_CUSTOM/plugins/you-should-use
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 sed -i '/^plugins=/d' ~/.zshrc
